cee07fcecd4d2bc534a347042070f70e49f1969b
Survingo <thehydra.tk@gmail.com>
2016-04-16 17:20:36 +0200

    added permissions

---
+++ src/pocketmine/permission/Permissible.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ *  ____ 
+ * / ___|  ___ _ __ _ __  _   _ _   _ 
+ * \___ \ / _ \ '__| '_ \| | | | | | |
+ *  ___) |  __/ |  | | | | |_| | |_| |
+ * |____/ \___|_|  |_| |_|\__, |\__,_|
+ *                        |___/       
+ * 
+*/
+
+namespace pocketmine\permission;
+
+use pocketmine\plugin\Plugin;
+
+interface Permissible extends ServerOperator{
+
+	/**
+	 * Checks if this instance has a permission overridden
+	 *
+	 * @param string|Permission $name
+	 *
+	 * @return boolean
+	 */
+	public function isPermissionSet($name);
+
+	/**
+	 * Returns the permission value if overridden, or the default value if not
+	 *
+	 * @param string|Permission $name
+	 *
+	 * @return mixed
+	 */
+	public function hasPermission($name);
+
+	/**
+	 * @param Plugin $plugin
+	 * @param string $name
+	 * @param bool   $value
+	 *
+	 * @return PermissionAttachment
+	 */
+	public function addAttachment(Plugin $plugin, $name = \null, $value = \null);
+
+	/**
+	 * @param PermissionAttachment $attachment
+	 *
+	 * @return void
+	 */
+	public function removeAttachment(PermissionAttachment $attachment);
+
+
+	/**
+	 * @return void
+	 */
+	public function recalculatePermissions();
+
+	/**
+	 * @return Permission[]
+	 */
+	public function getEffectivePermissions();
+
+}
--- src/pocketmine/permission/PermissionRemovedExecutor.php
+++ src/pocketmine/permission/PermissionRemovedExecutor.php
@@ -13,20 +13,12 @@
 namespace pocketmine\permission;
 
 
-interface ServerOperator{
-	/**
-	 * Checks if the current object has operator permissions
-	 *
-	 * @return bool
-	 */
-	public function isOp();
+interface PermissionRemovedExecutor{
 
 	/**
-	 * Sets the operator permission for the current object
-	 *
-	 * @param bool $value
+	 * @param PermissionAttachment $attachment
 	 *
 	 * @return void
 	 */
-	public function setOp($value);
+	public function attachmentRemoved(PermissionAttachment $attachment);
 }
