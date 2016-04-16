f9c0772adb9bade69b295e1858e86013505dd069
Survingo <thehydra.tk@gmail.com>
2016-04-16 18:16:55 +0200

    Level providers added

--- src/pocketmine/level/format/Chunk.php
+++ src/pocketmine/level/format/Chunk.php
@@ -1,1 +1,47 @@
-test
+<?php
+
+/*
+ *  ____ 
+ * / ___|  ___ _ __ _ __  _   _ _   _ 
+ * \___ \ / _ \ '__| '_ \| | | | | | |
+ *  ___) |  __/ |  | | | | |_| | |_| |
+ * |____/ \___|_|  |_| |_|\__, |\__,_|
+ *                        |___/       
+ * 
+*/
+
+namespace pocketmine\level\format;
+
+interface Chunk extends FullChunk{
+	const SECTION_COUNT = 8;
+
+	/**
+	 * Tests whether a section (mini-chunk) is empty
+	 *
+	 * @param $fY 0-7, (Y / 16)
+	 *
+	 * @return bool
+	 */
+	public function isSectionEmpty($fY);
+
+	/**
+	 * @param int $fY 0-7
+	 *
+	 * @return ChunkSection
+	 */
+	public function getSection($fY);
+
+	/**
+	 * @param int          $fY 0-7
+	 * @param ChunkSection $section
+	 *
+	 * @return boolean
+	 */
+	public function setSection($fY, ChunkSection $section);
+
+	/**
+	 * @return ChunkSection[]
+	 */
+	public function getSections();
+
+}
